469c93cd53a66505a9bde15557b4fe1e09f2aed4
aripo99 <a_polakof@umail.ucsb.edu>
2023-10-28 21:00:52 -0700

    Add transpiling and show the result in the browser

--- components/Browser.tsx
+++ components/Browser.tsx
@@ -1,9 +1,46 @@
+"use client";
+
+import React from 'react';
+import { transform } from '@babel/standalone';
+
+const transpileCode = (code: string) => {
+    return transform(code, {
+        presets: ['react']
+    }).code;
+};
+
+
 const Browser = () => {
+    const code = `
+        const App = () => {
+            return (
+            <div>
+                <h1>Hello, world!</h1>
+            </div>
+            );
+        }
+        
+        ReactDOM.render(<App />, document.getElementById('root'));
+        `;
+    const transpiledCode = transpileCode(code);
+    const fullCode = `
+      <div id="root"></div>
+      <script src="https://unpkg.com/react@17/umd/react.development.js"></script>
+      <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
+      <script>
+        ${transpiledCode}
+      </script>
+    `;
+
     return (
-        <div className="">
-            Browser
-        </div>
+        <iframe
+            srcDoc={fullCode}
+            title="browser-output"
+            className="w-full h-full"
+            sandbox="allow-scripts"
+        ></iframe>
     );
-}
+};
+
 
 export default Browser;
